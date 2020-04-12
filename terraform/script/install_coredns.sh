cfda446c2859c55f5e829800f26ec556d3dcbcd8
mickey <mickey.shaul@gmail.com>
2020-04-12 17:31:21 +0300

    update coredns install script

--- terraform/script/install_coredns.sh
+++ terraform/script/install_coredns.sh
@@ -4,11 +4,17 @@
 wget https://github.com/coredns/coredns/releases/download/v1.6.9/coredns_1.6.9_linux_amd64.tgz
 wget https://github.com/coredns/coredns/releases/download/v1.6.9/coredns_1.6.9_linux_amd64.tgz.sha256
 
+# Print checksum
 shasum -a 256 coredns_1.6.9_linux_amd64.tgz
 cat coredns_1.6.9_linux_amd64.tgz.sha256
 
+# Untar and move binary to path
 tar -zxvf coredns_1.6.9_linux_amd64.tgz
 sudo mv coredns /usr/local/bin/coredns
 
+# Copy zone file
 sudo mkdir -p /etc/coredns/zones
+git clone https://github.com/mickeysh/opsschool-coredns.git
+cd opsschool-coredns
 sudo cp db.opsschool.example /etc/coredns/zones
+
